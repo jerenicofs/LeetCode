1529ba42cf52d1e1925df1fd576cc4da933360e0
Jerenico Franssen Semanuel <149137626+jerenicofs@users.noreply.github.com>
2024-10-10 21:10:03 +0700

    Time: 0 ms (100%), Space: 10.7 MB (36.66%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if(nums.empty()) return 0;
+
+        int ct = nums.size();
+        int i = 0;
+        // for(i = 0; i < nums.size(); i++){
+        //     if(nums[i] == val){
+        //         nums.erase(nums.begin() + i);
+        //         i = 0;
+        //         ct--;
+        //     }
+        // }
+
+        while(i < nums.size()){
+            // cout << "current nums in i = " << i << "\n";
+            // for(int j = 0; j < nums.size(); j++){
+            //     cout << nums[j] <<", ";
+            // }
+            // cout << "\n";
+
+            if(nums[i] == val){
+                // cout << "Enter if in i = " << i << "\n";
+                nums.erase(nums.begin() + i);
+                ct--;
+            }
+            else{
+                i++;
+            }
+        }
+        // cout << i;
+        // cout << ct;
+        return ct;
+    }
+};
