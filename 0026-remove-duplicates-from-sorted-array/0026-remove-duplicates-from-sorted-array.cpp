b8abea384edab5febc969d5f4bd3fde23111b164
Jerenico Franssen Semanuel <149137626+jerenicofs@users.noreply.github.com>
2024-10-10 20:30:53 +0700

    Time: 12 ms (23.06%), Space: 21.3 MB (85.15%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.empty()) return 0;
+
+        int curr = 1;
+        for(int i = 1; i < nums.size() ; i++){
+            if(nums[i] != nums[i - 1]){
+                nums[curr] = nums[i];
+                curr++;
+            }
+        }
+        // cout << curr;
+        return curr;
+    }
+};
