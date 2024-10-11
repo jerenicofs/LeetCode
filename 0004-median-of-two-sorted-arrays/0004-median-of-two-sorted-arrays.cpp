95a317d8e254354ede846d992c372d01a92b30a2
Jerenico Franssen Semanuel <149137626+jerenicofs@users.noreply.github.com>
2024-10-11 18:04:09 +0700

    Time: 27 ms (48.65%), Space: 96 MB (16.43%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> merge;
-        int i = 0;
-        int j = 0;
+        // vector<int> merge;
+        // int i = 0;
+        // int j = 0;
 
-        while(i < nums1.size() && j < nums2.size()){
-            if(nums1[i] < nums2[j]){
-                merge.push_back(nums1[i]);
-                i++;
-            }
-            else{
-                merge.push_back(nums2[j]);
-                j++;
-            }
-        }
+        // while(i < nums1.size() && j < nums2.size()){
+        //     if(nums1[i] < nums2[j]){
+        //         merge.push_back(nums1[i]);
+        //         i++;
+        //     }
+        //     else{
+        //         merge.push_back(nums2[j]);
+        //         j++;
+        //     }
+        // }
 
-        while(i < nums1.size()){
-            merge.push_back(nums1[i]);
-            i++;
-        }
+        // while(i < nums1.size()){
+        //     merge.push_back(nums1[i]);
+        //     i++;
+        // }
         
-        while(j < nums2.size()){
-            merge.push_back(nums2[j]);
-            j++;
-        }
+        // while(j < nums2.size()){
+        //     merge.push_back(nums2[j]);
+        //     j++;
+        // }
 
+        // int n = merge.size();
+        // if(n%2 == 0) return (merge[n/2 - 1] + merge[n/2]) / 2.0;
+        // else return merge[n/2];
+
+        vector<int>merge;
+        if(nums1.empty() && nums2.empty()) return NULL;
+        else if(nums1.empty()) merge = nums2;
+        else if(nums2.empty()) merge = nums1;
+        else{
+            merge = nums2;
+            merge.insert(merge.end(), nums1.begin(), nums1.end());
+        }
+        sort(merge.begin(), merge.end());
         int n = merge.size();
         if(n%2 == 0) return (merge[n/2 - 1] + merge[n/2]) / 2.0;
         else return merge[n/2];
