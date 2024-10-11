b4663394379a1f7d3e0042faf87d1dc82b418dc0
Jerenico Franssen Semanuel <149137626+jerenicofs@users.noreply.github.com>
2024-10-11 18:45:17 +0700

    Time: 24 ms (61.35%), Space: 95.4 MB (36.7%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,47 +4,47 @@ public:
         
         // First Method
         
-        // vector<int> merge;
-        // int i = 0, j = 0, size1 = nums1.size(), size2 = nums2.size();
+        vector<int> merge;
+        int i = 0, j = 0, size1 = nums1.size(), size2 = nums2.size();
 
-        // while(i < size1 && j < size2){
-        //     if(nums1[i] < nums2[j]){
-        //         merge.push_back(nums1[i]);
-        //         i++;
-        //     }
-        //     else{
-        //         merge.push_back(nums2[j]);
-        //         j++;
-        //     }
-        // }
+        while(i < size1 && j < size2){
+            if(nums1[i] < nums2[j]){
+                merge.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                merge.push_back(nums2[j]);
+                j++;
+            }
+        }
 
-        // while(i < size1){
-        //     merge.push_back(nums1[i]);
-        //     i++;
-        // }
+        while(i < size1){
+            merge.push_back(nums1[i]);
+            i++;
+        }
         
-        // while(j < size2){
-        //     merge.push_back(nums2[j]);
-        //     j++;
-        // }
+        while(j < size2){
+            merge.push_back(nums2[j]);
+            j++;
+        }
 
-        // int n = merge.size();
-        // if(n%2 == 0) return (merge[n/2 - 1] + merge[n/2]) / 2.0;
-        // else return merge[n/2];
+        int n = merge.size();
+        if(n%2 == 0) return (merge[n/2 - 1] + merge[n/2]) / 2.0;
+        else return merge[n/2];
 
         // Second Method
 
-        vector<int>merge;
-        if(nums1.empty() && nums2.empty()) return 0;
-        else if(nums1.empty()) merge = nums2;
-        else if(nums2.empty()) merge = nums1;
-        else{
-            merge = nums2;
-            merge.insert(merge.end(), nums1.begin(), nums1.end());
-        }
-        sort(merge.begin(), merge.end());
-        int n = merge.size();
-        if(n%2 == 0) return ((double)(merge[n/2 - 1] + merge[n/2]) / 2);
-        else return merge[n/2];
+        // vector<int>merge;
+        // if(nums1.empty() && nums2.empty()) return 0;
+        // else if(nums1.empty()) merge = nums2;
+        // else if(nums2.empty()) merge = nums1;
+        // else{
+        //     merge = nums2;
+        //     merge.insert(merge.end(), nums1.begin(), nums1.end());
+        // }
+        // sort(merge.begin(), merge.end());
+        // int n = merge.size();
+        // if(n%2 == 0) return ((double)(merge[n/2 - 1] + merge[n/2]) / 2);
+        // else return merge[n/2];
     }
 };
