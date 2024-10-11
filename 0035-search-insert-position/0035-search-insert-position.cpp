d6337b2abbd35a2df57c9694be9fbe9135b188de
Jerenico Franssen Semanuel <149137626+jerenicofs@users.noreply.github.com>
2024-10-11 19:26:26 +0700

    Time: 2 ms (74.27%), Space: 12.2 MB (83.85%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,11 +1,28 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int size = nums.size(), mark = 0;
-        for(int i = 0 ; i < size; i++){
-            if(nums[i] == target) return i;
-            else if(nums[i] < target) mark = i + 1;
+        // linear search
+
+        // int size = nums.size(), mark = 0;
+        // for(int i = 0 ; i < size; i++){
+        //     if(nums[i] == target) return i;
+        //     else if(nums[i] < target) mark = i + 1;
+        // }
+
+        // return mark;
+
+        // binary search
+        int size = nums.size();
+        int left = 0, right = size - 1;
+ 
+        while( left <= right){
+            int mid = (left + right) / 2;
+        
+            if(nums[mid] == target) return mid;
+            
+            if(nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
         }
-        return mark;
+        return left;
     }
 };
