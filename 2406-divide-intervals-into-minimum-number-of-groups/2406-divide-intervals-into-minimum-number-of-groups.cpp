b623c1415ed7c991e370aa67e594d61d6548091a
Jerenico Franssen Semanuel <149137626+jerenicofs@users.noreply.github.com>
2024-10-12 22:28:00 +0700

    Time: 411 ms (27.03%), Space: 96.8 MB (68.69%) - LeetHub

---
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        vector<int>start, end;
+        int n = intervals.size();
+        for(int i = 0; i < n; i++){
+            start.push_back(intervals[i][0]);
+            end.push_back(intervals[i][1]);
+        }
+
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+        for(int i = 0; i < n; i++){
+            cout <<start[i] << ",";
+            cout << end[i];
+            cout << "\n";
+        }
+        int size = start.size(), track = 0, ct = 0;
+        for(int i = 0; i < size; i++){
+            if(end[track]  <  start[i]){
+                cout<< "Masuk if\n";
+                track++;
+            }
+            else ct++;
+            cout << "Current track: " << track;
+        }
+        cout << ct;
+
+        return ct;
+    }
+};
