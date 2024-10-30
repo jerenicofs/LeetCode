3915344bf9bdad381cfaa7e462f8b14e5e8dc933
Jerenico Franssen Semanuel <149137626+jerenicofs@users.noreply.github.com>
2024-10-31 01:21:07 +0700

    Time: 6 ms (44.15%), Space: 10 MB (16.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // if(s.size() != t.size()) return false;
-
-        // unordered_map<char, int> m;
-        // int n = max(s.size(), t.size());
-
-        // for(int i = 0; i < n; i++){
-        //     m[s[i]]++;
-        //     m[t[i]]--;
-        // }
-        // for(auto &pair : m){
-        //     if(pair.second != 0) return false;
-        // }
-
         if(s.size() != t.size()) return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(s != t) return false;
+
+        unordered_map<char, int> m;
+        int n = max(s.size(), t.size());
+
+        for(int i = 0; i < n; i++){
+            m[s[i]]++;
+            m[t[i]]--;
+        }
+        for(auto &pair : m){
+            if(pair.second != 0) return false;
+        }
+
         return true;
+
+        // if(s.size() != t.size()) return false;
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // if(s != t) return false;
+        // return true;
     }
 };
