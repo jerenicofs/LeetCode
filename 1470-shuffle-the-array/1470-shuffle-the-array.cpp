b658349fc8772dbc43ba639dfed77461380d32f7
Jerenico Franssen Semanuel <149137626+jerenicofs@users.noreply.github.com>
2024-10-30 21:19:00 +0700

    Time: 4 ms (61.1%), Space: 12.6 MB (8.47%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -4,7 +4,8 @@ public:
         vector<int> ans;
         vector<int> y;
         int mid = nums.size() / 2;
-        for(int i = mid; i < nums.size(); i++) y.push_back(nums[i]);
+        int z = nums.size();
+        for(int i = mid; i < z; i++) y.push_back(nums[i]);
         for(int i = 0; i < n; i++){
             ans.push_back(nums[i]);
             ans.push_back(y[i]);
