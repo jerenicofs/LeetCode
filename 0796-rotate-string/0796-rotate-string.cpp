798366ae7ee8bb1a954b863aeefa594112c44541
Jerenico Franssen Semanuel <149137626+jerenicofs@users.noreply.github.com>
2024-11-03 14:07:13 +0700

    Time: 0 ms (100%), Space: 8.8 MB (16.05%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         
-        // int n = s.size();
-        // for(int i = 0; i < n; i++){
-        //     string shift = s.substr(1) + s[0];
-        //     s = shift;
-        //     if(shift == goal) return true;
-        // }
-        // return false;
+        int n = s.size();
+        for(int i = 0; i < n; i++){
+            string shift = s.substr(1) + s[0];
+            s = shift;
+            if(shift == goal) return true;
+        }
+        return false;
         
-        if(s.size() != goal.size()) return false;
-        if((s+s).find(goal) != string::npos) return true;
-        else return false;
+        // if(s.size() != goal.size()) return false;
+        // if((s+s).find(goal) != string::npos) return true;
+        // else return false;
     }
 };
