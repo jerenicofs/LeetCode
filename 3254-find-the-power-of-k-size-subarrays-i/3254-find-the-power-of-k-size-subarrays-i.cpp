b38174b9cc63cfd926b10ac9b8dcc58affc79cbf
Jerenico Franssen Semanuel <149137626+jerenicofs@users.noreply.github.com>
2024-11-16 20:15:49 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    bool checkSort(vector<int> check){
+        for(int i = 0; i < check.size() - 1; i++){
+            if(check[i] >= check[i + 1]) return false;
+        }
+        return true;
+    }
+
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        if(nums.size() < 2) return nums;
+        vector<int> ans;
+        vector<int> temp;
+        for(int i = 0; i < k; i++){
+            temp.push_back(nums[i]);
+        }
+
+        for(int i = k; i <= nums.size(); i++){
+            if(checkSort(temp)) ans.push_back(temp.back());
+            else ans.push_back(-1);
+            if(i != nums.size()){
+                temp.erase(temp.begin());
+                temp.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
