780ba77f1179e522cccf3292de459fe0a151744f
Jerenico Franssen Semanuel <149137626+jerenicofs@users.noreply.github.com>
2024-11-20 18:26:43 +0700

    Time: 0 ms (100%), Space: 10.6 MB (91.91%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        if(nums.empty()) return 0;
+        // if(nums.empty()) return 0;
 
-        int ct = nums.size();
-        int i = 0;
-        // for(i = 0; i < nums.size(); i++){
+        // int ct = nums.size();
+        // int i = 0;
+        // // for(i = 0; i < nums.size(); i++){
+        // //     if(nums[i] == val){
+        // //         nums.erase(nums.begin() + i);
+        // //         i = 0;
+        // //         ct--;
+        // //     }
+        // // }
+
+        // while(i < nums.size()){
+        //     // cout << "current nums in i = " << i << "\n";
+        //     // for(int j = 0; j < nums.size(); j++){
+        //     //     cout << nums[j] <<", ";
+        //     // }
+        //     // cout << "\n";
+
         //     if(nums[i] == val){
+        //         // cout << "Enter if in i = " << i << "\n";
         //         nums.erase(nums.begin() + i);
-        //         i = 0;
         //         ct--;
         //     }
+        //     else{
+        //         i++;
+        //     }
         // }
+        // // cout << i;
+        // // cout << ct;
+        // return ct;
 
-        while(i < nums.size()){
-            // cout << "current nums in i = " << i << "\n";
-            // for(int j = 0; j < nums.size(); j++){
-            //     cout << nums[j] <<", ";
-            // }
-            // cout << "\n";
-
-            if(nums[i] == val){
-                // cout << "Enter if in i = " << i << "\n";
-                nums.erase(nums.begin() + i);
-                ct--;
-            }
-            else{
-                i++;
-            }
+        int ans = nums.size();
+        vector<int> temp;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != val) temp.push_back(nums[i]);
+            else ans--;
         }
-        // cout << i;
-        // cout << ct;
-        return ct;
+        nums = temp;
+        return ans;
     }
 };
