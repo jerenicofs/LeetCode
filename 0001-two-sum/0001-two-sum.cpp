f71f7c03de69c1c401d3328124bca32ef5ae0aca
Jerenico Franssen Semanuel <149137626+jerenicofs@users.noreply.github.com>
2026-02-05 23:40:21 +0700

    Time: 6 ms (43.96%), Space: 14.8 MB (52.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        unordered_map<int, int> map;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+
+            if(map.find(target - nums[i]) != map.end()){
+                return {map[target - nums[i]], i};
             }
+
+            map[nums[i]] = i;
         }
         return {};
     }
